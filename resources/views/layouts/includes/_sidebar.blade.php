e01d588ab980e4bea6a76387c7b9fabf3bd527e9
RianKNG <93078631+RianKNG@users.noreply.github.com>
2021-11-25 01:04:59 +0700

    Update _sidebar.blade.php
    menambahkan file di sidebar

--- resources/views/layouts/includes/_sidebar.blade.php
+++ resources/views/layouts/includes/_sidebar.blade.php
@@ -3,7 +3,7 @@
         <nav>
             <ul class="nav">
                 <li><a href="/" class=""><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
-                <li><a href="/ba" class=""><i class="lnr lnr-home"></i> <span>LamSurat</span></a></li>
+                <li><a href="/ba" class=""><i class="lnr lnr-home"></i> <span>LampSurat</span></a></li>
                 <li><a href="/surat" class=""><i class="lnr lnr-envelope"></i> <span>Surat</span></a></li>
                 <li><a href="/layanan" class=""><i class="lnr lnr-cog"></i> <span>LayananData</span></a></li>
                 <li><a href="/exportpdf" onclick ="alert('yakin ingin donload')" class=""><i class="lnr lnr-download"></i> <span>Exportpdf</span></a></li>  
